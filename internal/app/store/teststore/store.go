f97b6c88248b5668c04826dbd35ba274ff0eac0f
Денис Харке <hda007@campus.mephi.ru>
2022-07-19 00:42:25 +0300

    create user func and create func in test store

--- internal/app/store/teststore/store.go
+++ internal/app/store/teststore/store.go
@@ -1,5 +1,19 @@
 package teststore
 
+import "github.com/Dennikoff/TodoAPI/internal/app/model"
+
 type Store struct {
-	userRepository
+	userRepository *UserRepository
 }
+
+func (s *Store) Users() *UserRepository {
+	if s.userRepository == nil {
+		s.userRepository = &UserRepository{
+			store: s,
+			users: make(map[string]*model.User),
+		}
+	}
+	return s.userRepository
+}
+
+//TODO: add create and User func
