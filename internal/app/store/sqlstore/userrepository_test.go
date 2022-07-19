c825dc3fb775c9c348ddd1232a864ad608d93a82
Денис Харке <hda007@campus.mephi.ru>
2022-07-19 15:16:47 +0300

    add tests to sqlstore find by email

--- internal/app/store/sqlstore/userrepository_test.go
+++ internal/app/store/sqlstore/userrepository_test.go
@@ -18,3 +18,20 @@ func TestUserRepository_Create(t *testing.T) {
 	assert.NoError(t, store.User().Create(user))
 	assert.NotNil(t, user.ID)
 }
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	db, del := sqlstore.TestDB(t, databaseURL)
+	defer del("users")
+
+	user := model.TestUser()
+	store := sqlstore.New(db)
+
+	_, err := store.User().FindByEmail(user.Email)
+	assert.Error(t, err)
+
+	assert.NoError(t, store.User().Create(user))
+
+	us, err := store.User().FindByEmail(user.Email)
+	assert.NotNil(t, us)
+	assert.NoError(t, err)
+}
