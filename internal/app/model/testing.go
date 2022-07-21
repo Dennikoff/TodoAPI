8be91605b03bf527c3ee215843199bcf8cf8d312
Денис Харке <hda007@campus.mephi.ru>
2022-07-21 11:47:25 +0300

    create todo

--- internal/app/model/testing.go
+++ internal/app/model/testing.go
@@ -1,8 +1,18 @@
 package model
 
+import "time"
+
 func TestUser() *User {
 	return &User{
 		Email:    "user@example.org",
 		Password: "password",
+	}
+}
+
+func TestTodo() *Todo {
+	return &Todo{
+		Header:      "Test Header",
+		Text:        "Test Text",
+		CreatedDate: time.Now(),
 	}
 }
--- internal/app/model/todo.go
+++ internal/app/model/todo.go
@@ -1,1 +1,10 @@
 package model
+
+import "time"
+
+type Todo struct {
+	ID          int       `json:"id"`
+	Header      string    `json:"header"`
+	Text        string    `json:"text"`
+	CreatedDate time.Time `json:"createdDate"`
+}
